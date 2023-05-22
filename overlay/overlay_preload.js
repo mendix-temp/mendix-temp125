2f8e81d1f7a48dc479040555c9b61781129b934b
Baranger <louis.baranger@siemens.com>
2023-05-22 15:21:34 +0200

    Restructure folder layout

--- overlay/overlay_preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  open_app: (name, url) => ipcRenderer.send('open_app_overlay', name, url),
-  handle_json: (jsonData) => ipcRenderer.on('json', jsonData)
-})
