b0a9ae52f79453bc6b535db3c61e62398ca2cf7a
mendix-temp <133011381+mendix-temp@users.noreply.github.com>
2024-06-07 12:18:05 -0400

    Delete src directory

--- src/login/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-// Expose part of electron API to login.js
-contextBridge.exposeInMainWorld('electronAPI', {
-    update_station: (stationUrl) => ipcRenderer.send('update_station', stationUrl),
-    no_config: (callback) => ipcRenderer.on('no_config', callback),
-    quit_app: () => ipcRenderer.send('quit_app')
-})
